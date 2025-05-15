5db13f0fb70d60e5beddf75b1fe7f959a17bfec1
renzhong <1852568062@qq.com>
2025-05-15 09:17:57 +0800

    fix

--- data/schoolGuide.js
+++ data/schoolGuide.js
@@ -7,15 +7,21 @@
 
 export const school_guide = [
   {
-    title: '待编辑',
-    content: '待编辑21321312',
-    imageList: [''],
-    keywords: ['介绍']
+      title: '河北科技师范学院2024 - 2025学年校历',
+      content: '涵盖2024 - 2025学年学校的教学安排、节假日、考试周等时间信息，帮助师生合理规划学习和生活。',
+      imageList: ['https://gampusgo-1321866016.cos.ap-beijing.myqcloud.com/images_maps/xiaoli_1.png'],
+      keywords: ['校历', '教学时间安排', '学期规划']
   },
   {
-    title: '待编辑2',
-    content: '待编辑212312',
-    imageList: [''],
-    keywords: ['图书']
-  }
-] 
+      title: '图书馆开关时间',
+      content: '详细介绍图书馆的开放和关闭时间，包括工作日、周末以及节假日的特殊安排，方便学生借阅和自习。',
+      imageList: ['https://gampusgo-1321866016.cos.ap-beijing.myqcloud.com/images_maps/tsgtime.png'],
+      keywords: ['图书馆', '开放时间', '借阅时间']
+  },
+  {
+      title: '秦皇岛校区图书馆馆藏分布',
+      content: '说明秦皇岛校区图书馆各楼层、各区域的图书类别分布，帮助读者快速找到所需书籍。',
+      imageList: ['https://gampusgo-1321866016.cos.ap-beijing.myqcloud.com/images_maps/gcfb.png'],
+      keywords: ['图书馆馆藏', '校区分布', '图书查找']
+  },
+]
