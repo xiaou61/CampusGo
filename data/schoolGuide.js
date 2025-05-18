20a979539d26360108589a85a1df1a3bcb8ad247
aliyun1153623907 <3153566913@qq.com>
2025-05-18 21:41:12 +0800

    fixxxxxx

--- data/schoolGuide.js
+++ data/schoolGuide.js
@@ -6,22 +6,52 @@
  */
 
 export const school_guide = [
-  {
-      title: '河北科技师范学院2024 - 2025学年校历',
-      content: '涵盖2024 - 2025学年学校的教学安排、节假日、考试周等时间信息，帮助师生合理规划学习和生活。',
-      imageList: ['https://gampusgo-1321866016.cos.ap-beijing.myqcloud.com/images_maps/xiaoli_1.png'],
-      keywords: ['校历', '教学时间安排', '学期规划']
-  },
-  {
-      title: '图书馆开关时间',
-      content: '详细介绍图书馆的开放和关闭时间，包括工作日、周末以及节假日的特殊安排，方便学生借阅和自习。',
-      imageList: ['https://gampusgo-1321866016.cos.ap-beijing.myqcloud.com/images_maps/tsgtime.png'],
-      keywords: ['图书馆', '开放时间', '借阅时间']
-  },
-  {
-      title: '秦皇岛校区图书馆馆藏分布',
-      content: '说明秦皇岛校区图书馆各楼层、各区域的图书类别分布，帮助读者快速找到所需书籍。',
-      imageList: ['https://gampusgo-1321866016.cos.ap-beijing.myqcloud.com/images_maps/gcfb.png'],
-      keywords: ['图书馆馆藏', '校区分布', '图书查找']
-  },
-]
+    {
+        title: '河北科技师范学院2024 - 2025学年校历',
+        content: '涵盖2024 - 2025学年学校的教学安排、节假日、考试周等时间信息，帮助师生合理规划学习和生活。',
+        imageList: ['https://gampusgo-1321866016.cos.ap-beijing.myqcloud.com/guide/xiaoli_1.png'],
+        keywords: ['校历', '教学时间安排', '学期规划']
+    },
+    {
+        title: '图书馆开关时间',
+        content: '详细介绍图书馆的开放和关闭时间，包括工作日、周末以及节假日的特殊安排，方便学生借阅和自习。',
+        imageList: ['https://gampusgo-1321866016.cos.ap-beijing.myqcloud.com/guide/tsgtime.png'],
+        keywords: ['图书馆', '开放时间', '借阅时间']
+    },
+    {
+        title: '秦皇岛校区图书馆馆藏分布',
+        content: '说明秦皇岛校区图书馆各楼层、各区域的图书类别分布，帮助读者快速找到所需书籍。',
+        imageList: ['https://gampusgo-1321866016.cos.ap-beijing.myqcloud.com/guide/gcfb.png'],
+        keywords: ['图书馆馆藏', '校区分布', '图书查找']
+    },
+    {
+      title: '水卡办理与充值',
+      content: '办理地址：北院第二教学楼（东、西教学楼）中间的【大过道】墙上挂有红色【直饮水卡办理处】指示牌和【自助售卡机】\n1.方式：扫描【自助售卡机】上面的二维码支付10元，即可自动\n2.充值:用手机微信扫描水卡背面二维码即可充值”',
+      imageList: ['https://gampusgo-1321866016.cos.ap-beijing.myqcloud.com/guide/shuika.jpg'],
+      keywords: ['水卡', '水卡办理', '水卡充值']
+    },
+    {
+      title: '浴室开放时间',
+      content: '9：00-22:00',
+      imageList: ['https://gampusgo-1321866016.cos.ap-beijing.myqcloud.com/guide/yuchi.jpg'],
+      keywords: ['浴池']
+    },
+    {
+      title: '快递物理地址与营业时间',
+      content: '地址：河北省秦皇岛市海港区白塔岭街道河北大街西段360号\n京东物流营业时间：9：00-18：00\n菜鸟驿站营业时间：8：00-20：00',
+      keywords: ['快递', '快递地址']
+    },
+    {
+      title: '怎么连接校园网',
+      content: '无线名称：HNUST-Student，账号是学号，默认密码是身份证后六位，最后一位是字母用0代替。',
+      imageList: ['https://gampusgo-1321866016.cos.ap-beijing.myqcloud.com/guide/wifi.jpg'],
+      keywords: ['校园网', 'wifi']
+    },
+    {
+      title: '宿舍设施有问题如何报修',
+      content: '遇到维修问题时通过网上智能报修平台报修，微信公众号为河北科技师范学院微后勤。报修时请将损坏照片及具体位置填写清楚以便于维修师傅携带相关工具及配件。',
+      imageList: ['https://gampusgo-1321866016.cos.ap-beijing.myqcloud.com/guide/houqin.jpg'],
+      keywords: ['后勤', '宿舍维修','维修']
+    },
+  ]
+  
